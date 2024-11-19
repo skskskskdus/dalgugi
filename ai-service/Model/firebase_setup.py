7a999f9cd65a63f5a338c06d83a596cfb789a775
rlaskdus <skdus021121@gmail.com>
2024-11-19 12:23:14 +0900

    update

--- ai-service/Model/firebase_setup.py
+++ ai-service/Model/firebase_setup.py
@@ -1,7 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials,firestore
 
-cred = credentials.Certificate("transitai-firebase-adminsdk-t5n10-9832a4f222.json")
+cred = credentials.Certificate("transitai-firebase-adminsdk-t5n10-5de6d0555b.json")
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
